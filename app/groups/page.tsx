8f9a505ee0a6466906cd796fb6cf95a67ebda2c6
Leo Alfred <leojalfred@gmail.com>
2024-02-05 21:52:02 -0600

    Add page for Groups

---
+++ app/groups/page.tsx
@@ -0,0 +1,14 @@
+import { createClient } from '@/utils/supabase/server'
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+
+export default async function Groups() {
+  const cookieStore = cookies()
+  const supabase = createClient(cookieStore)
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
+  if (!session) redirect('/login')
+
+  return <div></div>
+}
