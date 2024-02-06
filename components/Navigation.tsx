b6799078aa279c2fadf15f5c772fb1d1819eb16e
Leo Alfred <leojalfred@gmail.com>
2024-02-05 21:49:48 -0600

    Adapt Navigation to handle more links

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -2,21 +2,48 @@
 
 import { Button, buttonVariants } from '@/components/ui/button'
 import { logOut } from '@/lib/actions'
+import { cn } from '@/lib/utils'
 import { type Session } from '@supabase/supabase-js'
 import { LogIn, LogOut } from 'lucide-react'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 interface NavigationProps {
   session: Session | null
 }
 
 export default function Navigation({ session }: NavigationProps) {
+  const pathname = usePathname()
+
+  const linkData = [
+    {
+      href: '/groups',
+      text: 'Groups',
+    },
+  ]
+  const links = linkData.map(({ href, text }, index) => {
+    const isActive = pathname === href
+    const classes = cn(
+      'text-zinc-400 transition-colors hover:text-white',
+      isActive && 'text-white',
+    )
+
+    return (
+      <Link key={index} href={href} className={classes}>
+        {text}
+      </Link>
+    )
+  })
+
   return (
     <nav className="fixed top-0 flex h-16 w-full justify-center border-b border-b-foreground/10">
       <div className="flex w-full max-w-4xl items-center justify-between p-3 text-sm">
-        <h2 className="text-xl">
-          <Link href="/">Fotomo</Link>
-        </h2>
+        <div className="flex items-center">
+          <h2 className="mr-4 text-xl sm:mr-12">
+            <Link href="/">Fotomo</Link>
+          </h2>
+          <div className="space-x-4 sm:space-x-6">{links}</div>
+        </div>
         {session ?
           <Button
             variant="outline"
